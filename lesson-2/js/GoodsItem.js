492d26cf4a7faeac9b639c12a8469ade78d2020c
Safuanov Ruslan <safuanov_ruslan_w@mail.ru>
2021-05-28 00:27:00 +0300

    lesson-2

---
+++ lesson-2/js/GoodsItem.js
@@ -0,0 +1,10 @@
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
+}
